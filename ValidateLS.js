12af2174f107fb0849f5a134b14e06b0633c0632
Om Dalwadi <88398870+OmDalwadi@users.noreply.github.com>
2022-11-25 22:37:22 -0600

    Trying to Link .JS file to Login/Signup Page

--- ValidateLS.js
+++ ValidateLS.js
@@ -10,15 +10,15 @@ function Loginpage(event)
         
         
         var elements = event.currentTarget;
-        var logemail = elements[0].value; //Email
+        var email = elements[0].value; //Email
         
-        var logpass = elements[1].value; //Password
+        var pswd = elements[1].value; //Password
         
       
         
-        var regex_logemail = /^\w+@[a-zA-Z]+?\.[a-zA-Z]{2,3}$/;
+        var regex_email = /^\w+@[a-zA-Z]+?\.[a-zA-Z]{2,3}$/;
         
-        var regex_logpass = /[^\s]{6,}/;
+        var regex_pswd = /[^\s]{6,}/;
       
       
         
@@ -39,13 +39,13 @@ function Loginpage(event)
       
       
         
-        if (logemail == null || logemail == "") 
+        if (email == null || email == "") 
             {
                 textNode = document.createTextNode("Email address empty.");
                 msg_email.appendChild(textNode);
                 valid = false;
             } 
-        else if (regex_email.test(logemail) == false) 
+        else if (regex_email.test(email) == false) 
             {
                 textNode = document.createTextNode("Email address wrong format. example: username@somewhere.sth");
                 msg_email.appendChild(textNode);
@@ -55,20 +55,20 @@ function Loginpage(event)
             
 
 
-            if (logpass == null || logpass == "") 
+            if (pswd == null || pswd== "") 
             {
                 textNode = document.createTextNode("Password is empty.");
                 msg_pswd.appendChild(textNode);
                 valid = false;
         
             }
-            else if (regex_pswd.test(logpass) == false) 
+            else if (regex_pswd.test(pswd) == false) 
             {
                 textNode = document.createTextNode("Password is invaldi. Be sure to follow the rules(NO SPACE).");
                 msg_pswd.appendChild(textNode);
                 valid = false;
             }
-            else if (logpass.length < 6) 
+            else if (pswd.length < 6) 
             {
                 textNode = document.createTextNode("Password should be of 6 character or longer.");
                 msg_pswd.appendChild(textNode);
