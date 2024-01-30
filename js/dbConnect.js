95566cf8d25ee5fc53342d817689403279d93a18
Om Dalwadi <88398870+OmDalwadi@users.noreply.github.com>
2024-01-29 23:10:21 -0600

    removed password

--- js/dbConnect.js
+++ js/dbConnect.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'm@y9h5H2',
+    password: 'Password for your database',
 });
 
 function insertData(data) {
